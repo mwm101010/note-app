ed4fbde04d296f2f28b24ab5f3e221644b15400a
herangakr <ma.muthukuda@gmail.com>
2019-09-28 17:31:49 +0000

    Create Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <div>
+      Nav Componenet
+      </div>
+    );
+  }
+
+}
+
+export default Nav;
