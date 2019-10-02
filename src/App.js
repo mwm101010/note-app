6b2ee90e26f57ed67c98885b4938e8fb9337cdce
herangakr <ma.muthukuda@gmail.com>
2019-10-02 14:32:02 +0000

    Perform HTTP requests via using axios to communicate with backend

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
@@ -12,17 +13,23 @@ class App extends Component {
     };
   }
 
-  toggleNote = () => {
-  this.setState({ showNote: ! this.state.showNote });
+  toggleNote = () => { 
+    this.setState({ showNote: ! this.state.showNote 
+    });
   }
 
+  getNotes = () => {
+    axios.get('https://note-api-muthuku.herokuapp.com/notes')
+    .then((res) => console.log(res.data) )
+    .catch((err) => console.log(err.response.data) );
+  }
 
 render() {
   const { showNote } = this.state;
   return (
     <div className="App">
       <Nav toggleNote={this.toggleNote} showNote={showNote}/>
-      { showNote ? <Note /> : <List /> }
+      { showNote ? <Note /> : <List getNotes={this.getNotes} /> }
     </div>
   );
 }
