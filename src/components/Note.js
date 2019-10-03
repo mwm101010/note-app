64be244acc680e75fd32da16dd4ea2409f6bddca
herangakr <ma.muthukuda@gmail.com>
2019-10-03 23:28:45 +0000

    Added a button bottom of the note to tag an another tag

--- src/components/Note.js
+++ src/components/Note.js
@@ -10,27 +10,42 @@ class Note extends React.Component {
     this.props.submitNote(formData, this.props.note.id);
   }
 
+  renderTagForm() {
+    return (
+      <span>
+        Tag your note:
+        <i className="tag-button material-icons">add circle</i>
+      </span>
+    );
+  };
+
 
   render() {
-    const {note} = this.props;
-    return(
+    const { note } = this.props;
+
+    return (
       <div className="note-container">
         <form className="note-form" onSubmit={(e) => this.onSubmit(e)}>
-          <input 
-            className="note-title-input" 
-            type="text" 
-            placeholder="Note Title...." 
-            defaultValue={note.title} 
+          <input
+            className="note-title-input"
+            type="text"
+            placeholder="Note Title..."
+            defaultValue={note.title}
             ref={(input) => this.title = input}
-          />
+           />
           <textarea
             className="note-textarea"
             placeholder="Type Here..."
             defaultValue={note.content}
             ref={(input) => this.content = input}
           />
-          <input className="note-button" type="submit" value="submit" />
+          <input className="note-button" type="submit" value="Submit" />
         </form>
+        <div className="tag-container">
+          <div className="tag-button-container">
+            {this.renderTagForm()}
+          </div>
+        </div>
       </div>
     );
   }
