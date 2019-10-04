91aae5bc9bc77990b6477dd2ed7af10b4eb47ca0
herangakr <ma.muthukuda@gmail.com>
2019-10-04 00:35:27 +0000

    Add button tag will display only for existing notes and do not display once new note creates, and also taging data are ready to post to backend API

--- src/components/Note.js
+++ src/components/Note.js
@@ -10,14 +10,43 @@ class Note extends React.Component {
     this.props.submitNote(formData, this.props.note.id);
   }
 
-  renderTagForm() {
-    return (
-      <span>
-        Tag your note:
-        <i className="tag-button material-icons">add circle</i>
-      </span>
-    );
-  };
+  onTagSubmit(e) {
+    e.preventDefault();
+    const formData = {
+      name: this.name.value
+    };
+    this.props.submitTag(formData, this.props.note.id)
+    this.props.closeTagForm();
+  }
+
+  renderTagForm(note) {
+    if(note.id !== undefined) {
+      if(!this.props.newTag) {
+        return (
+          <span>
+            Tag your note:
+            <i 
+              className="tag-button material-icons"
+              onClick={() => this.props.showTagForm()}
+            >
+              add circle
+            </i>
+          </span>
+        );
+      } else {
+        return (
+          <form onSubmit={(e) => this.onTagSubmit(e)}>
+            <input
+              className="tag-input"
+              type="text"
+              placeholder="Tag Name...."
+              ref={(input) => this.name = input}
+            />
+          </form>
+          );
+      }
+    }
+  }
 
 
   render() {
@@ -43,7 +72,7 @@ class Note extends React.Component {
         </form>
         <div className="tag-container">
           <div className="tag-button-container">
-            {this.renderTagForm()}
+            {this.renderTagForm(note)}
           </div>
         </div>
       </div>
